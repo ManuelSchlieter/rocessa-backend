08e2b992df0cd48ef4f4c275576a36d9bb4c3b13
mschlieter <m.schlieter@fabnamix.com>
2024-09-04 01:33:09 +0000

    - added persons repository

---
+++ src/main/java/org/rocessa/features/persons/PersonsRepository.java
@@ -0,0 +1,16 @@
+package org.rocessa.features.persons;
+
+import java.util.List;
+
+import org.rocessa.features.persons.models.Person;
+
+public interface PersonsRepository {
+
+    public void loadPersons();
+
+    List<Person> getAllPersons();
+
+    Person getPersonById(int id);
+
+    List<Person> getPersonsByColor(String color);
+}
---
+++ src/main/java/org/rocessa/features/persons/PersonsRepositoryCsv.java
@@ -0,0 +1,130 @@
+package org.rocessa.features.persons;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.rocessa.features.persons.models.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Repository;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import jakarta.annotation.PostConstruct;
+
+@Repository
+public class PersonsRepositoryCsv implements PersonsRepository {
+    private static final Logger logger = LoggerFactory.getLogger(PersonsRepositoryCsv.class);
+
+    Map<String, String> idToColorMap = Map.of(
+            "1", "blau",
+            "2", "grün",
+            "3", "violett",
+            "4", "rot",
+            "5", "gelb",
+            "6", "türkis",
+            "7", "weiß");
+
+    ResourceLoader resourceLoader;
+    List<Person> persons = new ArrayList<>();
+
+    @Autowired
+    public PersonsRepositoryCsv(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
+    @PostConstruct
+    public void init() {
+        loadPersons();
+    }
+
+    @Override
+    public List<Person> getAllPersons() {
+        return persons;
+    }
+
+    @Override
+    public Person getPersonById(int id) {
+        return persons.stream()
+                .filter(person -> person.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public List<Person> getPersonsByColor(String color) {
+        return persons.stream()
+                .filter(person -> person.getColor() == color)
+                .toList();
+    }
+
+    @Override
+    public void loadPersons() {
+        Resource resource = resourceLoader.getResource("classpath:sample-input.csv");
+        try (CSVReader reader = new CSVReader(new InputStreamReader(resource.getInputStream()))) {
+
+            int currentId = 0;
+            String[] entry;
+            while ((entry = reader.readNext()) != null) {
+                currentId++;
+                Person person = parsePersonFromCsvEntry(currentId, entry);
+                if (person != null) {
+                    persons.add(person);
+                }
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (CsvValidationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected Person parsePersonFromCsvEntry(int id, String[] entry) {
+        if (entry.length != 4) {
+            logger.warn("Malformed csv entry, skipping " + Arrays.toString(entry));
+            return null;
+        }
+
+        entry = trimEntries(entry);
+
+        var color = idToColorMap.get(entry[3]);
+        if (color == null) {
+            logger.warn("Malformed color entry, skipping " + Arrays.toString(entry));
+            return null;
+        }
+
+        var location = entry[2].split(" ", 2);
+        if (location.length < 2) {
+            logger.warn("Malformed location entry, skipping " + Arrays.toString(entry));
+            return null;
+        }
+
+        var person = new Person();
+        person.setId(id);
+        person.setLastname(entry[0]);
+        person.setName(entry[1]);
+        person.setZipcode(location[0]);
+        person.setCity(location[1]);
+        person.setColor(color);
+
+        return person;
+    }
+
+    private String[] trimEntries(String[] entry) {
+        return Arrays.stream(entry)
+                .map(String::trim)
+                .toArray(String[]::new);
+    }
+}
